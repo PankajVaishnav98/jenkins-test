6152c78f33dcd96234c83e209709a7e911054e2c
Coder98 <45947025+PankajVaishnav98@users.noreply.github.com>
2021-05-23 03:13:04 +0000

    pankaj : updated JenkinsTestApplication.java

--- src/main/java/com/pankaj/learning/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/pankaj/learning/jenkinstest/JenkinsTestApplication.java
@@ -8,7 +8,7 @@ public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
-		System.out.println("Just testing jenkins");
+		System.out.println("testing poll scm");
 	}
 
 }
